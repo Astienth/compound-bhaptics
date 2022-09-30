3c9a6124c28845b8a64477051a70d0cec183b479
Astienth <astienth@gmail.com>
2022-09-30 14:52:11 +0200

    adapting florian's code

--- Compound_bhaptics/MyBhapticsTactsuit.cs
+++ Compound_bhaptics/MyBhapticsTactsuit.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using MelonLoader;
+using UnityEngine;
 
 namespace MyBhapticsTactsuit
 {
@@ -12,12 +13,7 @@ namespace MyBhapticsTactsuit
     {
         public bool suitDisabled = true;
         public bool systemInitialized = false;
-
-        public bool faceConnected = false;
-        public bool armsConnected = false;
         private static ManualResetEvent HeartBeat_mrse = new ManualResetEvent(false);
-        private static ManualResetEvent Water_mrse = new ManualResetEvent(false);
-        private static ManualResetEvent Choking_mrse = new ManualResetEvent(false);
         public Dictionary<String, FileInfo> FeedbackMap = new Dictionary<String, FileInfo>();
 
         private static bHaptics.RotationOption defaultRotationOption = new bHaptics.RotationOption(0.0f, 0.0f);
@@ -43,9 +39,6 @@ namespace MyBhapticsTactsuit
             LOG("Starting HeartBeat thread...");
             Thread HeartBeatThread = new Thread(HeartBeatFunc);
             HeartBeatThread.Start();
-            LOG("Check if face or arms are connected...");
-            if (bHaptics.IsDeviceConnected(bHaptics.DeviceType.Tactal)) faceConnected = true;
-            if (bHaptics.IsDeviceConnected(bHaptics.DeviceType.Tactosy_arms)) armsConnected = true;
         }
 
         public void LOG(string logStr)
@@ -103,7 +96,48 @@ namespace MyBhapticsTactsuit
             bHaptics.SubmitRegistered(key, key, scaleOption, rotationOption);
         }
 
-        public void GunRecoil(bool isRightHand, float intensity = 1.0f, bool twoHanded = false, bool supportHand = false)
+        public static KeyValuePair<float, float> getAngleAndShift(Transform player, Vector3 hit)
+        {
+            // bhaptics pattern starts in the front, then rotates to the left. 0° is front, 90° is left, 270° is right.
+            // y is "up", z is "forward" in local coordinates
+            Vector3 patternOrigin = new Vector3(0f, 0f, 1f);
+            Vector3 hitPosition = hit - player.position;
+            Quaternion myPlayerRotation = player.rotation;
+            Vector3 playerDir = myPlayerRotation.eulerAngles;
+            // get rid of the up/down component to analyze xz-rotation
+            Vector3 flattenedHit = new Vector3(hitPosition.x, 0f, hitPosition.z);
+
+            // get angle. .Net < 4.0 does not have a "SignedAngle" function...
+            float hitAngle = Vector3.Angle(flattenedHit, patternOrigin);
+            // check if cross product points up or down, to make signed angle myself
+            Vector3 crossProduct = Vector3.Cross(flattenedHit, patternOrigin);
+            if (crossProduct.y < 0f) { hitAngle *= -1f; }
+            // relative to player direction
+            float myRotation = hitAngle - playerDir.y;
+            // switch directions (bhaptics angles are in mathematically negative direction)
+            myRotation *= -1f;
+            // convert signed angle into [0, 360] rotation
+            if (myRotation < 0f) { myRotation = 360f + myRotation; }
+
+
+            // up/down shift is in y-direction
+            // in Battle Sister, the torso Transform has y=0 at the neck,
+            // and the torso ends at roughly -0.5 (that's in meters)
+            // so cap the shift to [-0.5, 0]...
+            float hitShift = hitPosition.y;
+            //tactsuitVr.LOG("HitShift: " + hitShift.ToString());
+            float upperBound = 0.5f;
+            float lowerBound = -0.5f;
+            if (hitShift > upperBound) { hitShift = 0.5f; }
+            else if (hitShift < lowerBound) { hitShift = -0.5f; }
+            // ...and then spread/shift it to [-0.5, 0.5], which is how bhaptics expects it
+            else { hitShift = (hitShift - lowerBound) / (upperBound - lowerBound) - 0.5f; }
+
+            // No tuple returns available in .NET < 4.0, so this is the easiest quickfix
+            return new KeyValuePair<float, float>(myRotation, hitShift);
+        }
+
+        public void GunRecoil(bool isRightHand, float intensity = 1.0f, bool twoHanded = false)
         {
             float duration = 1.0f;
             var scaleOption = new bHaptics.ScaleOption(intensity, duration);
@@ -111,36 +145,11 @@ namespace MyBhapticsTactsuit
             string postfix = "_L";
             string otherPostfix = "_R";
             if (isRightHand) { postfix = "_R"; otherPostfix = "_L"; }
-            string keyArm = "Recoil" + postfix;
             string keyVest = "RecoilVest" + postfix;
-            string keyHands = "RecoilHands" + postfix;
-            string keyArmOther = "Recoil" + otherPostfix;
             string keyVestOther = "RecoilVest" + otherPostfix;
             string keyHandsOther = "RecoilHands" + otherPostfix;
-            bHaptics.SubmitRegistered(keyHands, keyHands, scaleOption, rotationFront);
-            bHaptics.SubmitRegistered(keyArm, keyArm, scaleOption, rotationFront);
             bHaptics.SubmitRegistered(keyVest, keyVest, scaleOption, rotationFront);
-            if (twoHanded || supportHand)
-            {
-                bHaptics.SubmitRegistered(keyHandsOther, keyHandsOther, scaleOption, rotationFront);
-                bHaptics.SubmitRegistered(keyArmOther, keyArmOther, scaleOption, rotationFront);
-            }
             if (twoHanded) bHaptics.SubmitRegistered(keyVestOther, keyVestOther, scaleOption, rotationFront);
-        }
-
-        public void SwordRecoil(bool isRightHand, float intensity = 1.0f)
-        {
-            float duration = 1.0f;
-            var scaleOption = new bHaptics.ScaleOption(intensity, duration);
-            var rotationFront = new bHaptics.RotationOption(0f, 0f);
-            string postfix = "_L";
-            if (isRightHand) { postfix = "_R"; }
-            string keyArm = "Sword" + postfix;
-            string keyVest = "SwordVest" + postfix;
-            string keyHands = "RecoilHands" + postfix;
-            bHaptics.SubmitRegistered(keyHands, keyHands, scaleOption, rotationFront);
-            bHaptics.SubmitRegistered(keyArm, keyArm, scaleOption, rotationFront);
-            bHaptics.SubmitRegistered(keyVest, keyVest, scaleOption, rotationFront);
         }
 
         public void HeadShot(float hitAngle)
@@ -155,15 +164,6 @@ namespace MyBhapticsTactsuit
             else { PlayBackHit("BulletHit", hitAngle, 0.5f); }
         }
 
-        public void FootStep(bool isRightFoot)
-        {
-            if (!bHaptics.IsDeviceConnected(bHaptics.DeviceType.Tactosy_feet)) { return; }
-            string postfix = "_L";
-            if (isRightFoot) { postfix = "_R"; }
-            string key = "FootStep" + postfix;
-            PlaybackHaptics(key);
-        }
-
         public void StartHeartBeat()
         {
             HeartBeat_mrse.Set();
@@ -172,27 +172,6 @@ namespace MyBhapticsTactsuit
         public void StopHeartBeat()
         {
             HeartBeat_mrse.Reset();
-        }
-
-        public void StartWater()
-        {
-            Water_mrse.Set();
-        }
-
-        public void StopWater()
-        {
-            Water_mrse.Reset();
-        }
-
-        public void StartChoking()
-        {
-            Choking_mrse.Set();
-        }
-
-        public void StopChoking()
-        {
-            Choking_mrse.Reset();
-            bHaptics.TurnOff("TeleportOpened");
         }
 
         public bool IsPlaying(String effect)
@@ -217,8 +196,6 @@ namespace MyBhapticsTactsuit
         public void StopThreads()
         {
             StopHeartBeat();
-            StopWater();
-            StopChoking();
         }
 
 
